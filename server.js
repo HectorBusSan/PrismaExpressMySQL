2f51384fc76deee7ea93b92c476824a8ab990a6d
Hector <hector141599@gmail.com>
2022-05-12 11:14:41 -0500

    Agregando get al CRUD

--- server.js
+++ server.js
@@ -6,9 +6,45 @@ const port = process.env.PORT || 3000
 const {PrismaClient}=require('@prisma/client');
 const prisma = new PrismaClient();
 
-app.get('/',(req,res)=>{
+app.get('/', async (req,res)=>{
     res.json({message:'alive'});
 });
 app.listen(port,()=>{
-    console.log(`Listeningto request on port ${port}`)
+    console.log(`Listeningto request on port ${port}`);
 });
+
+// CRUD
+app.get("/explorers",async(req,res)=>{
+    const allExplorers = await prisma.explorer.findMany({});
+    res.json(allExplorers);
+})
+
+app.get("/explorers/:id", async (req,res) =>{
+    const id= req.params.id;
+    const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+    res.json(explorer);
+});
+
+app.post("/explorers",async (req,res)=>{
+    const explorer={
+        name: req.body.name,
+        username: req.body.username,
+        mission: req.body.mission
+    };
+    const message = "Explorer creado";
+    await prisma.explorer.create({data:explorer});
+    return res.json({message});
+});
+
+app.put("/explorers",async (req,res)=>{
+    const id= parseInt(req.params.id);
+    await prisma.explorer.update({
+        where:{
+            id:id
+        },
+        data:{
+            mission:req.body.mission
+        }
+    })
+    return res.json({message:"Actualizado correctamente"});
+})
