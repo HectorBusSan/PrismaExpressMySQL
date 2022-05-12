a584e48cca6299284dc408a740963a5daa18ea1d
Hector <hector141599@gmail.com>
2022-05-12 10:56:17 -0500

    Agregando server.js listen and get

---
+++ server.js
@@ -0,0 +1,14 @@
+const express= require('express');
+const app= express();
+app.use(express.json());
+const port = process.env.PORT || 3000
+
+const {PrismaClient}=require('@prisma/client');
+const prisma = new PrismaClient();
+
+app.get('/',(req,res)=>{
+    res.json({message:'alive'});
+});
+app.listen(port,()=>{
+    console.log(`Listeningto request on port ${port}`)
+});
